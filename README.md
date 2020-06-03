985c425b95e235353016ac7cf0933f9f4bc0b7a8
hugues <ghugues@netc.fr>
2020-06-03 08:49:14 +0200

    Create writer.go

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # go-logoutput
-A wrater that change output file every day
+
+A writer that change output file every day
+
+## Exemple
+
+Create a directory and the file `2006-01-02.log` into.
+
+```go
+package main
+
+import (
+	"github.com/HuguesGuilleus/go-logoutput"
+	"log"
+)
+
+func main() {
+	logoutput.SetLog("log")
+	log.Println("Hello World")
+}
+```
---
+++ writer.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2020, Hugues GUILLEUS <ghugues@netc.fr>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package logoutput
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const day time.Duration = time.Hour * 24
+
+// A logger output. You can use Zero value.
+type W struct {
+	// The directory taht contains all log file.
+	Root  string
+	today time.Time
+	f     *os.File
+}
+
+// Create a W writer
+func New(root string) *W {
+	w := &W{Root: root}
+	w.open()
+	return w
+}
+
+// Set the output of the standard log.
+func SetLog(root string) {
+	log.SetOutput(New(root))
+}
+
+// Write the content of data to the day file and into os.Stdout
+func (w *W) Write(data []byte) (int, error) {
+	os.Stdout.Write(data)
+	w.open()
+	return w.f.Write(data)
+}
+
+// Check the date of the opened file, and open it if necessary.
+func (w *W) open() {
+	now := time.Now().Truncate(day)
+	if now.Equal(w.today) && w.f != nil {
+		return
+	}
+
+	w.today = now
+	if w.f != nil {
+		w.f.Close()
+	}
+
+	name := filepath.Join(w.Root, w.today.Format("2006-01-02.log"))
+	os.MkdirAll(filepath.Dir(name), 0774)
+	var err error
+	w.f, err = os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0664)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
